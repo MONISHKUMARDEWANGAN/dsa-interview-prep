cc6bdee4867907025b0ba18ec6743c119f7d7576
MONISHKUMARDEWANGAN <dmonish53@gmail.com>
2026-08-26 18:42:22 +0530

    Time: 3 ms (22.24%), Space: 263.4 MB (15.66%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
+        int n=nums.size();
         k = k%n;
+
         reverse(nums.begin(), nums.end());
-        
-        int l=0, r=k-1;
-        while(l<r) {
-            swap(nums[l], nums[r]);
-            l++;
-            r--;
-        }
-        l=k, r=n-1;
-        while(l<r) {
-            swap(nums[l], nums[r]);
-            l++;
-            r--;
-        }
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
     }
 };
