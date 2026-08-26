5e0ff4ecfa0b1c06fa03d8da6e270c649ad986c9
MONISHKUMARDEWANGAN <dmonish53@gmail.com>
2026-08-26 17:59:42 +0530

    Time: 0 ms (100%), Space: 11.7 MB (84.56%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -4,7 +4,7 @@ public:
         int k=0;
         for(int i=0; i<nums.size(); i++) {
             if(nums[i]!=val) {
-                nums[k++] = nums[i];
+                nums[k++]=nums[i];
             }
         }
         return k;
