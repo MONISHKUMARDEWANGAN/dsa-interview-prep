c464868c79a1ca6710506c26b13b73a0f9070dc7
MONISHKUMARDEWANGAN <dmonish53@gmail.com>
2026-08-27 11:00:15 +0530

    Time: 5 ms (38.59%), Space: 22 MB (93.35%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -2,37 +2,33 @@ class Solution {
 public:
     bool validPalindrome(string s) {
         int l=0, r=s.size()-1;
-        bool isL=true, isR=true;
+        int lS=true, rS=true;
         while(l<r) {
             if(s[l]!=s[r]) {
                 break;
-            }else {
-                l++;
-                r--;
             }
-        }
-        
-        int l1=l+1, r1=r;
-        while(l1<r1) {
-            if(s[l1]!=s[r1]) {
-                isL = false;
-                break;
-            }else {
-                l1++;
-                r1--;
-            }
+            l++;
+            r--;
         }
 
-        int l2=l, r2=r-1;
+        int l1 = l+1, r1 = r;
+        while(l1<r1) {
+            if(s[l1]!=s[r1]) {
+                lS=false;
+                break;
+            }
+            l1++;
+            r1--;
+        }
+
+        int l2 = l, r2 = r-1;
         while(l2<r2) {
             if(s[l2]!=s[r2]) {
-                isR = false;
-                break;
-            }else {
-                l2++;
-                r2--;
+                rS=false;
             }
+            l2++;
+            r2--;
         }
-        return isL || isR;
+        return lS || rS;
     }
 };
